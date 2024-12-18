0801a9559a8af01c10b75f098f9b37366741522a
WesleyJunkins <Wjunkins13@yahoo.com>
2024-12-18 12:24:56 -0600

    uncommitted changes

---
+++ zzz.py
@@ -0,0 +1,17 @@
+from cyberpi import *
+
+cyberpi.display.clear()
+
+cyberpi.console.print("Hello World!")
+
+mbot2.drive_power(0, 0)
+
+
+
+# cyberpi.mbot2.forward(50, 10)
+
+# cyberpi.mbot2.turn_left(50, 10)
+
+# mbot2.drive_power(100, -100)
+
+# mbot2.drive_speed(200, 200)
